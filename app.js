aa63228126a0db907006e1cd0379b5871fea21e8
dorlevko <dorlevkovich@gmail.com>
2020-10-23 00:30:21 +0300

    HTML+CSS

--- app.js
+++ app.js
@@ -1,22 +1,11 @@
-const express = require('express');
-const app = express();
 
-const menu = document.querySelector("#mobile-menu");
+const menu = document.querySelector('#mobile_menu');
 const menuLinks = document.querySelector('.navbar__menu');
 
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
-});
-
-app.get("/",function(req,res){
-    res.send('<h1>Welcome to our app</h1>')
-});
-
-let port = process.env.PORT||3500;
-app.listen(port,function() {
-    console.log(`server is listening on port ${port}`);
 });
 
 
